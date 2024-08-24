f548e24f5f03edbacfe825a7c1e00a0e1b63c1c1
Matt-Verni <matthewpmendez@gmail.com>
2024-08-24 18:42:11 -0400

    Added functions

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# readme-gen
-This application will generate professional README file from user input
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)<br>
+<h1 align="center">User Generated README File</h1><br> 
+### Description
+### Users are asked some key questions. Based on their responses the README file is createed
+### Table of Contents
+
+
+### Installation
+No installation is required.
+ 
+### Usage
+Answer the questions to generate the file.
+### Contribution Guidelines
+You can contribute freely.
+
+### License
+https://opensource.org/licenses/MIT<br>
+
+### Questions
+If you have any questions, please visit my GitHub profile or email me.
+-  GitHub profile:Plutarch1971<br>
+-  Email: matthew@gmail.com<br>
+
+### Screenshots
+X-Screenshot: Purpose and Function
+    
+![Alt text](assets/screenshot/screenshot.png)
+  
+### Application Video
+- x-screencastify
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,37 @@
+// Generate Table of Contents based on selected sections
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenses = { 
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    Apache: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    GPL: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    BSD: '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    None: ''
+  };
+  if (!license) {
+    return '';
+  }
+  return licenses[license];
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenses = { 
+    MIT: 'https://opensource.org/licenses/MIT',
+    Apache: 'https://opensource.org/licenses/Apache-2.0',
+    GPL: 'https://www.gnu.org/licenses/gpl-3.0',
+    BSD: 'https://opensource.org/licenses/BSD-3-Clause',
+    None: ''
+  };
+  if (!license) {
+    return '';
+  }
+  return licenses[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,9 +39,55 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // let tableOfContents = '';
+  
+  // if (data.toc.includes('Installation')) {
+  //     tableOfContents += '- [Installation](#installation)\n';
+  // }
+  // if (data.toc.includes('Usage')){
+  //     tableOfContents += '- [Usage](#usage)\n';
+  // }
+  // if (data.toc.includes('Contributing')) {
+  //     tableOfContents += '- [Contributing](#contributing)\n';
+  // }
+  // if (data.toc.includes('Tests')) {
+  //     tableOfContents += '- [Tests](#tests)\n';
+  // }
+  // if (data.toc.includes('License')) {
+  //     tableOfContents += '- [License](#license)\n';
+  // }
+  return (`${renderLicenseBadge(data.license)}<br>
+<h1 align="center">${data.title}</h1><br> 
+### Description
+### ${data.description}
+### Table of Contents
 
-`;
+
+### Installation
+${data.installation}
+ 
+### Usage
+${data.usage}
+### Contribution Guidelines
+${data.contributing}
+
+### License
+${renderLicenseLink(data.license)}<br>
+
+### Questions
+If you have any questions, please visit my GitHub profile or email me.
+-  GitHub profile:${data.github}<br>
+-  Email: ${data.email}<br>
+
+### Screenshots
+X-Screenshot: Purpose and Function
+    
+![Alt text](assets/screenshot/screenshot.png)
+  
+### Application Video
+- x-screencastify`
+  );
 }
 
 export default generateMarkdown;
+
